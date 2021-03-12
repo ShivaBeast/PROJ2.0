e7a9af848b5d9e50f4b8287e3ddd3271f27f8df0
shiva kandhai <19039107@student.hhs.nl>
2021-03-12 17:59:42 +0100

    student

--- src/src/Student.java
+++ src/src/Student.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class Student {
+import java.util.Scanner;
+
+
+class Student {
+    private String name;
+    private String studentNummer;
+
+    public Student(String name,String studentNummer){
+        this.name = name;
+        this.studentNummer = studentNummer;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getStudentNummer() {
+        return this.studentNummer;
+    }
+
+    public void inloggen(){
+        Scanner scanner = new Scanner(System.in);
+        this.name = scanner.nextLine();
+        while(studentNummer.length() <= 0 || studentNummer.length() > 8){
+            this.studentNummer = scanner.nextLine();
+        }
+    }
 }
