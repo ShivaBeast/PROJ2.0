5a0db0ba777557b48337e62909bf3bf54e10bf37
Wishal Toekoen <wishal_wishnu@hotmail.com>
2021-03-17 10:52:18 +0100

    pressAnyKey() aangemaakt, kleine dingen aangepast

--- src/src/Main.java
+++ src/src/Main.java
@@ -5,11 +5,8 @@ public class Main {
     public static void main (String[] arg){
         Scanner scanner = new Scanner(System.in);
 
-
         Menu menu1 = new Menu();
         menu1.showMenu();
-
-
 
     }
 }
