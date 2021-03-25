17ae4487eb64c1fb649bc5ddd4469d287df9ad2d
Wishal Toekoen <wishal_wishnu@hotmail.com>
2021-03-25 10:58:24 +0100

    merge met pitt

--- src/src/ExamenAdministratie.java
+++ src/src/ExamenAdministratie.java
@@ -4,19 +4,21 @@ public class ExamenAdministratie {
     private Menu menu = new Menu();
     private Scanner reader = new Scanner(System.in);
 
+
     public void start() {
         menu.showMenu();
         String keuze = reader.nextLine();
 
-        while (!keuze.equals("8")) {
+        while (!keuze.equals("8")){
             menu.getChoice(keuze);
-            System.out.print("\nVoer je keuze in: ");
+            System.out.println("\nDruk op een knop om door te gaan...");
+            reader.nextLine();
+            menu.showMenu();
             keuze = reader.nextLine();
-        }
 
+        }
         stop();
     }
-
     private void stop() {
         System.out.println("Exit");
     }
