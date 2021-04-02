b700dac9945abaa4c991d90edef3a7331e73a849
shiva kandhai <19039107@student.hhs.nl>
2021-04-02 12:33:58 +0200

    regel 21 in examen test verbeterd

--- src/test/ExamenTest.java
+++ src/test/ExamenTest.java
@@ -18,7 +18,7 @@ public class ExamenTest {
     @Test
     public void TestGetVragen(){
         ArrayList<Vraag> vragen = new ArrayList<Vraag>();
-        vragen.add(new Vraag());
+        vragen.add(new Vraag("hoe heet jij?","Pitt Khotchada", 10));
         Examen examen = new Examen("Biologie", vragen);
         Assertions.assertEquals(vragen, examen.getVragen());
 
