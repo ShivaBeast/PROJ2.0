389bc07326f4fe766ddfc81ee9cc75a8c8e30acb
robbertsteggerda <20216092@student.hhs.nl>
2021-04-05 17:47:43 +0200

    test Poging toegevoegd

--- src/test/PogingTest.java
+++ src/test/PogingTest.java
@@ -1,11 +1,17 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
 
 public class PogingTest {
 
+    //test getGeslaagd() denk ik idk
     @Test
     public void TestPoging(){
-
+        ArrayList<Poging> pogingenVanStudenten = new ArrayList<>();
+        Poging poging = new Poging(true);
+        pogingenVanStudenten.add(poging);
+        Poging poging2 = pogingenVanStudenten.get(0);
+        Assertions.assertTrue(poging2.getGeslaagd());
     }
 
 
